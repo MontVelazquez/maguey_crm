b961bab43535a064b4230d396ad29509c578533b
MontVelazquez <montserrat.velazquez.1988@gmail.com>
2022-06-23 08:44:50 -0500

    Conexion

--- php/registro/conexion.php
+++ php/registro/conexion.php
@@ -2,8 +2,10 @@
 	#$conexion = new mysqli("localhost","u434890810_maguey_123","Maguey_123@","u434890810_maguey_123");
 	$Server="localhost";
 	$User="root";
-	$Pass="";
-	$DataBase="crm";
+	#$Pass="";
+	$Pass="Maguey_test"
+	#$DataBase="crm";
+	$DataBase="u434890810_test_maguey";
 	
 	$conexion=mysqli_connect($Server,$User, $Pass,$DataBase);
 	//$conexion = new mysqli("localhost","crm","root","crm");
